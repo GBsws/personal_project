1f72947c1a339bf2c878aa809d2c94b9aced2619
Gabriela Biswasova <pracho.dayat@seznam.cz>
2024-04-26 19:27:14 +0200

    basic structure

---
+++ app/components/header.js
@@ -0,0 +1,13 @@
+import styles from './header.module.css'
+
+
+export default function Header(){
+  return(
+    <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>app/page.js</code>
+        </p>
+      </div>
+  )
+}
